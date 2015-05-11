1117c06d0086c711a934e6ba88decfaf23b00fc4
FrikkieSnyman <frikkie.snyman@live.co.za>
2015-05-11 17:16:37 +0200

    IT WERKS

--- code/Client.java
+++ code/Client.java
@@ -59,13 +59,13 @@ public class Client implements Runnable{
                 String htmlResponse = "<?xml version=\"1.0\"?><!DOCTYPE wml PUBLIC \"-//WAPFORUM//DTD WML 1.1//EN\" \"http://www.wapforum.org/DTD/wml_1.1.xml\"><wml><template>    <do type=\"accept\" label=\"Back\"><prev/></do></template><card id=\"home\" title=\"The Octane Group\"><p align=\"center\">   <img src=\"images/octane.wbmp\" alt=\"[Octane Logo]\"/></p><p><big><b>T</b></big>he Octane Group is an exceptional team of software engineersimmediately available to ignite the productivity of your company's engineering projects.<br/><big><b>O</b></big>ur mission is to leverage our software developmentexperience with the latest technologies to help you reduce costs,create new sources of revenue, and increase profits.<br/><big><b>W</b></big>e have a proven track record in developing a wide rangeof software applications on a variety of platforms. From software for Internet ASPsto voiceXML processing to enterprise solutions, our experience with both existingand emerging technologies provides our clients with an unprecedented edge in software development.<br/><a href=\"assets.wml\">Assets</a><a href=\"benefits.wml\">Benefits</a><a href=\"experience.wml\">Experience</a><a href=\"clients.wml\">Clients</a><a href=\"team.wml\">Team</a><a href=\"contact.wml\">Contact</a><br/><small>The Octane Group, LLC &#169; 2001</small></p></card></wml>";
             
                 // PrintStream pout = new PrintStream(connection.getOutputStream());
-                socketOut.println("HTTP/1.1 200 OK\r\n");
-                socketOut.println("Content-Type: text/vnd.wap.wml\r\n");
-                socketOut.println("Connection: Close\r\n");
-                socketOut.println("Content-length: " + htmlResponse.length() + "\r\n");
-                socketOut.println("Cache-control: no-cache\r\n\r\n");
-                socketOut.println(htmlResponse);                         
-                socketOut.flush(); 
+                socketOut.write("HTTP/1.1 200 OK\r\n");
+                socketOut.write("Content-Type: text/vnd.wap.wml\r\n");
+                socketOut.write("Connection: Close\r\n");
+                socketOut.write("Content-length: " + htmlResponse.length() + "\r\n");
+                socketOut.write("Cache-control: no-cache\r\n\r\n");
+                socketOut.write(htmlResponse);                         
+                socketOut.flush();
                 // connection.shutdownOutput();
 
 
